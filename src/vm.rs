89b1d945ea2970b1f7ac5e7538310e0fb2bfc8ee
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-23 04:18:02 +0330

    added: value operations

--- src/vm.rs
+++ src/vm.rs
@@ -2,11 +2,226 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 enum Value {
-    Variable(String),
     Number(i32),
     Float(f64),
     StringLiteral(String),
     RegexPattern(String),
+}
+
+impl Value {
+    fn add(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(Value::Number(a + b)),
+            (Value::Float(a), Value::Float(b)) => Some(Value::Float(a + b)),
+            (Value::StringLiteral(ref a), Value::StringLiteral(ref b)) => {
+                let mut concatenated = a.clone();
+                concatenated.push_str(b);
+                Some(Value::StringLiteral(concatenated))
+            }
+            _ => None,
+        }
+    }
+
+    fn subtract(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(Value::Number(a - b)),
+            (Value::Float(a), Value::Float(b)) => Some(Value::Float(a - b)),
+            _ => None,
+        }
+    }
+
+    fn multiply(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(Value::Number(a * b)),
+            (Value::Float(a), Value::Float(b)) => Some(Value::Float(a * b)),
+            _ => None,
+        }
+    }
+
+    fn divide(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => {
+                if b != 0 {
+                    Some(Value::Number(a / b))
+                } else {
+                    None
+                }
+            }
+            (Value::Float(a), Value::Float(b)) => {
+                if b != 0.0 {
+                    Some(Value::Float(a / b))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    fn exponentiate(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(base), Value::Number(exponent)) => {
+                Some(Value::Number(base.pow(*exponent as u32)))
+            }
+            (Value::Float(base), Value::Float(exponent)) => {
+                Some(Value::Float(base.powf(*exponent)))
+            }
+            _ => None,
+        }
+    }
+
+    fn equals(&self, other: &Value) -> bool {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => a == b,
+            (Value::Float(a), Value::Float(b)) => a == b,
+            (Value::StringLiteral(a), Value::StringLiteral(b)) => a == b,
+            _ => false,
+        }
+    }
+
+    fn not_equals(&self, other: &Value) -> bool {
+        !self.equals(other)
+    }
+
+    fn greater_than(&self, other: &Value) -> Option<bool> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(a > b),
+            (Value::Float(a), Value::Float(b)) => Some(a > b),
+            _ => None,
+        }
+    }
+
+    fn greater_than_equals(&self, other: &Value) -> Option<bool> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(a >= b),
+            (Value::Float(a), Value::Float(b)) => Some(a >= b),
+            _ => None,
+        }
+    }
+
+    fn less_than(&self, other: &Value) -> Option<bool> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(a < b),
+            (Value::Float(a), Value::Float(b)) => Some(a < b),
+            _ => None,
+        }
+    }
+
+    fn less_than_equals(&self, other: &Value) -> Option<bool> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(a <= b),
+            (Value::Float(a), Value::Float(b)) => Some(a <= b),
+            _ => None,
+        }
+    }
+
+    fn ere_match(&self, pattern: &Value) -> Option<bool> {
+        match (self, pattern) {
+            (Value::StringLiteral(input), Value::RegexPattern(regex)) => {
+                let regex = regex::Regex::new(regex).ok()?;
+                Some(regex.is_match(input))
+            }
+            _ => None,
+        }
+    }
+
+    fn ere_non_match(&self, pattern: &Value) -> Option<bool> {
+        match (self, pattern) {
+            (Value::StringLiteral(input), Value::RegexPattern(regex)) => {
+                let regex = regex::Regex::new(regex).ok()?;
+                Some(!regex.is_match(input))
+            }
+            _ => None,
+        }
+    }
+
+    fn increment(&mut self) -> Option<()> {
+        match self {
+            Value::Number(ref mut n) => {
+                *n = n.checked_add(1)?;
+                Some(())
+            }
+            Value::Float(ref mut f) => {
+                *f += 1.0;
+                Some(())
+            }
+            _ => None,
+        }
+    }
+
+    fn decrement(&mut self) -> Option<()> {
+        match self {
+            Value::Number(ref mut n) => {
+                *n = n.checked_sub(1)?;
+                Some(())
+            }
+            Value::Float(ref mut f) => {
+                *f -= 1.0;
+                Some(())
+            }
+            _ => None,
+        }
+    }
+
+    fn bitwise_not(&mut self) -> Option<Value> {
+        match self {
+            Value::Number(ref mut n) => Some(Value::Number(!(*n))),
+            _ => None,
+        }
+    }
+
+    fn make_negative(&mut self) -> Option<()> {
+        match self {
+            Value::Number(ref mut n) => {
+                *n = -(*n);
+                Some(())
+            }
+            Value::Float(ref mut f) => {
+                *f = -(*f);
+                Some(())
+            }
+            _ => None,
+        }
+    }
+
+    fn bitwise_and(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(Value::Number(a & b)),
+            _ => None,
+        }
+    }
+
+    fn bitwise_or(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(Value::Number(a | b)),
+            _ => None,
+        }
+    }
+
+    fn bitwise_xor(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Some(Value::Number(a ^ b)),
+            _ => None,
+        }
+    }
+
+    fn logical_or(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => {
+                Some(Value::Number(if a != &0 || b != &0 { 1 } else { 0 }))
+            }
+            _ => None,
+        }
+    }
+
+    fn logical_and(&self, other: &Value) -> Option<Value> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => {
+                Some(Value::Number(if a != &0 && b != &0 { 1 } else { 0 }))
+            }
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -26,6 +241,7 @@ enum Instruction {
     Sub,
     Mul,
     Rem,
+    Exp,
     Div,
     Shr,
     Shl,
@@ -37,9 +253,16 @@ enum Instruction {
     Le,
     And,
     Or,
+    Incr,
+    Decr,
+    Pos,
+    Neg,
+    EreMatch,
+    EreNonMatch,
     BitwiseAnd,
     BitwiseOr,
     BitwiseXor,
+    BitwiseNot,
     Print,
     Printf,
     Getline,
@@ -48,7 +271,6 @@ enum Instruction {
     AppendOut,
     RegexMatch,
 }
-
 
 #[derive(Debug, Clone)]
 struct StackVM {
@@ -67,5 +289,4 @@ impl StackVM {
             environ: HashMap::new(),
         }
     }
-
 }
