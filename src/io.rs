6a274cb3f6e8bed430ee20cfb8040a210c1834bc
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-31 01:39:11 +0330

    fixed: the delimiter/regex line get is improved, errors fixed, formatted

--- src/io.rs
+++ src/io.rs
@@ -1,14 +1,14 @@
+use io::Result;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
-use std::result::{Result, Result::Err};
 
 use regex::Regex;
 
 struct AwkIO {
     inputs: HashMap<String, Option<Box<dyn BufRead>>>,
     outputs: HashMap<String, Box<dyn Write>>,
-    tokenized: Vec<String>,
+    last_read: Vec<String>,
 }
 
 impl AwkIO {
@@ -16,15 +16,16 @@ impl AwkIO {
         Self {
             inputs: HashMap::new(),
             outputs: HashMap::new(),
+            last_read: vec![],
         }
     }
 
-    pub fn add_input(&mut self, file_path: &str) -> Result<(), Err> {
+    pub fn add_input(&mut self, file_path: &str) -> Result<()> {
         if file_path == "-" {
             self.inputs.insert("STDIN".to_string(), None);
             Ok(())
         } else {
-            let handle = File::open(file_path).map_err(|_| Err)?;
+            let handle = File::open(file_path).unwrap();
             let buffer = BufReader::new(handle);
             self.inputs
                 .insert(file_path.to_string(), Some(Box::new(buffer)));
@@ -32,28 +33,28 @@ impl AwkIO {
         }
     }
 
-    pub fn add_output(&mut self, file_path: &str) -> Result<(), Err> {
+    pub fn add_output(&mut self, file_path: &str) -> Result<()> {
         if file_path == "-" {
             self.outputs
                 .insert("STDOUT".to_string(), Box::new(io::stdout()));
             Ok(())
         } else {
-            let handle = File::create(file_path).map_err(|_| Err)?;
+            let handle = File::create(file_path).unwrap();
             let buffer = BufWriter::new(handle);
             self.outputs.insert(file_path.to_string(), Box::new(buffer));
             Ok(())
         }
     }
 
-    pub fn read_line_from_input(&mut self, file_path: &str) -> Result<Option<String>, Err> {
+    pub fn read_line_from_input(&mut self, file_path: &str) -> Result<Option<String>> {
         if let Some(input) = self.inputs.get_mut(file_path) {
             if let Some(input) = input {
                 let mut line = String::new();
-                input.read_line(&mut line).map_err(|_| Err)?;
+                input.read_line(&mut line).unwrap();
                 Ok(Some(line))
             } else {
                 let mut line = String::new();
-                io::stdin().read_line(&mut line).map_err(|_| Err)?;
+                io::stdin().read_line(&mut line).unwrap();
                 Ok(Some(line))
             }
         } else {
@@ -61,83 +62,58 @@ impl AwkIO {
         }
     }
 
-    pub fn read_from_input(&mut self, file_path: &str, buffer: &mut Vec<u8>) -> Result<usize, Err> {
+    pub fn read_from_input(&mut self, file_path: &str, buffer: &mut String) -> Result<usize> {
         if let Some(input) = self.inputs.get_mut(file_path) {
             if let Some(input) = input {
-                input.read(buffer).map_err(|_| Err)
+                input.read_line(buffer)
             } else {
-                io::stdin().read(buffer).map_err(|_| Err)
+                io::stdin().read_line(buffer)
             }
         } else {
             Ok(0)
         }
     }
 
-    pub fn write_to_output(&mut self, file_path: &str, data: &[u8]) -> Result<(), Err> {
+    pub fn write_to_output(&mut self, file_path: &str, data: &[u8]) -> Result<()> {
         if let Some(output) = self.outputs.get_mut(file_path) {
-            output.write_all(data).map_err(|_| Err)?;
+            output.write_all(data).unwrap();
             Ok(())
         } else {
             Ok(())
         }
     }
 
-    pub fn read_until_regex(
-        &mut self,
-        file_path: &str,
-        pattern: Regex,
-        buffer: &mut Vec<u8>,
-    ) -> Result<usize, Err> {
+    pub fn read_until_regex(&mut self, file_path: &str, pattern: Regex) -> Result<usize> {
         if let Some(input) = self.inputs.get_mut(file_path) {
-            if let Some(input) = input {
-                let mut read_buffer = Vec::new();
-                let mut delimiter_found = false;
+            let mut read_buffer = String::new();
+            let mut total_buffer = String::new();
+            let mut delimiter_found = false;
+            let mut bytes_read = 0;
 
-                while !delimiter_found {
-                    buffer.clear();
-                    let bytes_read = input.read_to_end(buffer).map_err(|_| Err)?;
-
-                    if bytes_read == 0 {
-                        break; // Reached the end of the file
-                    }
-
-                    read_buffer.extend_from_slice(buffer);
-
-                    // Convert the read data to a string for regex matching
-                    let content_str = String::from_utf8_lossy(&read_buffer);
-
-                    // Check if the delimiter is found
-                    if pattern.is_match(&content_str) {
-                        delimiter_found = true;
-                    }
+            while !delimiter_found {
+                if let Some(input) = input {
+                    bytes_read = input.read_line(&mut read_buffer)?;
+                } else {
+                    bytes_read = io::stdin().read_line(&mut read_buffer)?
                 }
 
-                self.tokenized = pattern.split(&buffer).collect();
-                Ok(read_buffer.len())
-            } else {
-                // STDIN
-                let mut delimiter_found = false;
-
-                while !delimiter_found {
-                    buffer.clear();
-                    let bytes_read = io::stdin().read_to_end(buffer).map_err(|_| Err)?;
-
-                    if bytes_read == 0 {
-                        break; // Reached the end of input
-                    }
-
-                    // Convert the read data to a string for regex matching
-                    let content_str = String::from_utf8_lossy(buffer);
-
-                    // Check if the delimiter is found
-                    if pattern.is_match(&content_str) {
-                        delimiter_found = true;
-                    }
+                if bytes_read == 0 {
+                    break;
                 }
 
-                self.tokenized = pattern.split(&buffer).collect();
-                Ok(buffer.len())
+                total_buffer.push_str(&read_buffer);
+
+                if pattern.is_match(&total_buffer) {
+                    delimiter_found = true;
+                }
+
+                self.last_read = pattern
+                    .split(&total_buffer)
+                    .map(|s| s.to_string())
+                    .collect();
             }
+
+            Ok(bytes_read)
         } else {
             Ok(0)
         }
