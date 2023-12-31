163b52f900df7a8b1550af8afd5eb401c48eb57d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-31 08:50:09 +0330

    added: fields

--- src/io.rs
+++ src/io.rs
@@ -1,14 +1,13 @@
 use io::Result;
+use regex::Regex;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 
-use regex::Regex;
-
 struct AwkIO {
     inputs: HashMap<String, Option<Box<dyn BufRead>>>,
     outputs: HashMap<String, Box<dyn Write>>,
-    last_read: Vec<String>,
+    fields: Vec<String>,
 }
 
 impl AwkIO {
@@ -16,7 +15,7 @@ impl AwkIO {
         Self {
             inputs: HashMap::new(),
             outputs: HashMap::new(),
-            last_read: vec![],
+            fields: vec![],
         }
     }
 
@@ -25,7 +24,7 @@ impl AwkIO {
             self.inputs.insert("STDIN".to_string(), None);
             Ok(())
         } else {
-            let handle = File::open(file_path).unwrap();
+            let handle = File::open(file_path)?;
             let buffer = BufReader::new(handle);
             self.inputs
                 .insert(file_path.to_string(), Some(Box::new(buffer)));
@@ -39,23 +38,34 @@ impl AwkIO {
                 .insert("STDOUT".to_string(), Box::new(io::stdout()));
             Ok(())
         } else {
-            let handle = File::create(file_path).unwrap();
+            let handle = File::create(file_path)?;
             let buffer = BufWriter::new(handle);
             self.outputs.insert(file_path.to_string(), Box::new(buffer));
             Ok(())
         }
     }
 
-    pub fn read_line_from_input(&mut self, file_path: &str) -> Result<Option<String>> {
+    pub fn read_line_from_input(
+        &mut self,
+        file_path: &str,
+        delimiter: char,
+    ) -> Result<Option<String>> {
         if let Some(input) = self.inputs.get_mut(file_path) {
-            if let Some(input) = input {
-                let mut line = String::new();
-                input.read_line(&mut line).unwrap();
+            let mut line = String::new();
+            match input {
+                Some(input) => input.read_line(&mut line)?,
+                None => io::stdin().read_line(&mut line)?,
+            }
+
+            if !line.is_empty() {
+                self.fields = line
+                    .trim()
+                    .split(delimiter)
+                    .map(|s| s.to_string())
+                    .collect();
                 Ok(Some(line))
             } else {
-                let mut line = String::new();
-                io::stdin().read_line(&mut line).unwrap();
-                Ok(Some(line))
+                Ok(None)
             }
         } else {
             Ok(None)
@@ -64,10 +74,9 @@ impl AwkIO {
 
     pub fn read_from_input(&mut self, file_path: &str, buffer: &mut String) -> Result<usize> {
         if let Some(input) = self.inputs.get_mut(file_path) {
-            if let Some(input) = input {
-                input.read_line(buffer)
-            } else {
-                io::stdin().read_line(buffer)
+            match input {
+                Some(input) => input.read_line(buffer)?,
+                None => io::stdin().read_line(buffer)?,
             }
         } else {
             Ok(0)
@@ -76,7 +85,7 @@ impl AwkIO {
 
     pub fn write_to_output(&mut self, file_path: &str, data: &[u8]) -> Result<()> {
         if let Some(output) = self.outputs.get_mut(file_path) {
-            output.write_all(data).unwrap();
+            output.write_all(data)?;
             Ok(())
         } else {
             Ok(())
@@ -91,10 +100,9 @@ impl AwkIO {
             let mut bytes_read = 0;
 
             while !delimiter_found {
-                if let Some(input) = input {
-                    bytes_read = input.read_line(&mut read_buffer)?;
-                } else {
-                    bytes_read = io::stdin().read_line(&mut read_buffer)?
+                match input {
+                    Some(input) => bytes_read = input.read_line(&mut read_buffer)?,
+                    None => bytes_read = io::stdin().read_line(&mut read_buffer)?,
                 }
 
                 if bytes_read == 0 {
@@ -107,7 +115,7 @@ impl AwkIO {
                     delimiter_found = true;
                 }
 
-                self.last_read = pattern
+                self.fields = pattern
                     .split(&total_buffer)
                     .map(|s| s.to_string())
                     .collect();
