95a0a386cdb301b8c3078b98f642571e72bd40f4
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-05 00:39:01 +0330

    moved: meta to main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-mod meta;
 mod value;
 mod machine;
 mod awkio;
 
+#[macro_export]
+macro_rules! exit_err {
+    ($reason:expr) => {
+            eprintln!("{}", $reason);
+            eprintln!("This caused RustyAWK to exit with status 1"); 
+            std::process::exit(1)
+    };
+
+    ($fmt:literal, $($arg:expr),+ $(,)?) => {
+            eprintln!($fmt, $($arg),+);
+            eprintln!("This caused RustyAWK to exit with status 1");
+            std::process::exit(1)
+    };
+}
 
 fn main() {
     println!("Hello, world!");
